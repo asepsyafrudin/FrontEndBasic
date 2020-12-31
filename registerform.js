f1e14ab14803d06c57ad329ec13e93e6db2ee908
Asep Syafrudin <asseeppsyafrudin@gmail.com>
2021-01-01 00:41:24 +0700

    Improve Pagination on RegisterForm

--- registerform.js
+++ registerform.js
@@ -2,7 +2,8 @@ let globalData = []
 
 //object data 
 
-function ObjectData(name , address, religion, hobby, gender) {
+function ObjectData(id , name , address, religion, hobby, gender) {
+    this.id = id
     this.name = name ;
     this.address = address;
     this.religion= religion;
@@ -10,36 +11,45 @@ function ObjectData(name , address, religion, hobby, gender) {
     this.gender = gender;
 }
 
-var mahasiswa1 = new ObjectData("Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
-var mahasiswa2 = new ObjectData("Hartono", "Bekasi", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
-var mahasiswa3 = new ObjectData("Fadil", "Bogor", "Islam" , ["Basket" , "Renang"], "Laki-Laki")
-var mahasiswa4 = new ObjectData("Revina", "Surabaya", "Islam" , ["Sepeda" , "Renang"], "Perempuan")
-var mahasiswa5 = new ObjectData("Syifa", "Malang", "Islam" , "Renang", "Perempuan")
-var mahasiswa6 = new ObjectData("Ali", "Bekasi", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
-var mahasiswa7 = new ObjectData("Nia", "Bandung", "Islam" , ["Sepeda" , "Renang"], "Perempuan")
-var mahasiswa8 = new ObjectData("Zazam", "Semarang", "Islam" , ["Sepeda" , "Basket", "Badminton"], "Laki-Laki")
-var mahasiswa9 = new ObjectData("Denny", "Bogor", "Islam" , ["Sepeda" , "Badminton"], "Laki-Laki")
-var mahasiswa10 = new ObjectData("Dina", "Bogor", "Islam" , ["Sepeda" , "Badminton"], "Laki-Laki")
-var mahasiswa11 = new ObjectData("Riady", "Bekasi", "Islam" , ["Sepeda" , "Renang", "Basket"], "Laki-Laki")
-var mahasiswa12 = new ObjectData("Cinta", "Pekalongan", "Islam" , ["Sepeda" , "Renang" , "Football"], "Perempuan")
-var mahasiswa13 = new ObjectData("Laras", "Madiun", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
-var mahasiswa14 = new ObjectData("Sarah", "DKI Jakarta", "Islam" , ["Sepeda" , "Badminton"], "Perempuan")
-var mahasiswa15 = new ObjectData("Tya", "DKI Jakarta", "Islam" , ["Sepeda" , "Basket"], "Perempuan")
-var mahasiswa16 = new ObjectData("Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
-var mahasiswa17 = new ObjectData("Hartono", "Bekasi", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
-var mahasiswa18 = new ObjectData("Fadil", "Bogor", "Islam" , ["Basket" , "Renang"], "Laki-Laki")
-var mahasiswa19 = new ObjectData("Revina", "Surabaya", "Islam" , ["Sepeda" , "Renang"], "Perempuan")
-var mahasiswa20 = new ObjectData("Syifa", "Malang", "Islam" , "Renang", "Perempuan")
-var mahasiswa21 = new ObjectData("Ali", "Bekasi", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
-var mahasiswa22 = new ObjectData("Nia", "Bandung", "Islam" , ["Sepeda" , "Renang"], "Perempuan")
-var mahasiswa23 = new ObjectData("Zazam", "Semarang", "Islam" , ["Sepeda" , "Basket", "Badminton"], "Laki-Laki")
-var mahasiswa24 = new ObjectData("Denny", "Bogor", "Islam" , ["Sepeda" , "Badminton"], "Laki-Laki")
-var mahasiswa25 = new ObjectData("Dina", "Bogor", "Islam" , ["Sepeda" , "Badminton"], "Laki-Laki")
-var mahasiswa26 = new ObjectData("Riady", "Bekasi", "Islam" , ["Sepeda" , "Renang", "Basket"], "Laki-Laki")
-var mahasiswa27 = new ObjectData("Cinta", "Pekalongan", "Islam" , ["Sepeda" , "Renang" , "Football"], "Perempuan")
-var mahasiswa28 = new ObjectData("Laras", "Madiun", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
-var mahasiswa29 = new ObjectData("Sarah", "DKI Jakarta", "Islam" , ["Sepeda" , "Badminton"], "Perempuan")
-var mahasiswa30 = new ObjectData("Tya", "DKI Jakarta", "Islam" , ["Sepeda" , "Basket"], "Perempuan")
+var mahasiswa1 = new ObjectData("1", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa2 = new ObjectData("2", "Hartono", "Bekasi", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
+var mahasiswa3 = new ObjectData("3", "Fadil", "Bogor", "Islam" , ["Basket" , "Renang"], "Laki-Laki")
+var mahasiswa4 = new ObjectData("4", "Revina", "Surabaya", "Islam" , ["Sepeda" , "Renang"], "Perempuan")
+var mahasiswa5 = new ObjectData("5", "Syifa", "Malang", "Islam" , "Renang", "Perempuan")
+var mahasiswa6 = new ObjectData("6", "Ali", "Bekasi", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
+var mahasiswa7 = new ObjectData("7", "Nia", "Bandung", "Islam" , ["Sepeda" , "Renang"], "Perempuan")
+var mahasiswa8 = new ObjectData("8","Zazam", "Semarang", "Islam" , ["Sepeda" , "Basket", "Badminton"], "Laki-Laki")
+var mahasiswa9 = new ObjectData("9", "Denny", "Bogor", "Islam" , ["Sepeda" , "Badminton"], "Laki-Laki")
+var mahasiswa10 = new ObjectData("10", "Dina", "Bogor", "Islam" , ["Sepeda" , "Badminton"], "Laki-Laki")
+var mahasiswa11 = new ObjectData("11", "Riady", "Bekasi", "Islam" , ["Sepeda" , "Renang", "Basket"], "Laki-Laki")
+var mahasiswa12 = new ObjectData("12", "Cinta", "Pekalongan", "Islam" , ["Sepeda" , "Renang" , "Football"], "Perempuan")
+var mahasiswa13 = new ObjectData("13", "Laras", "Madiun", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
+var mahasiswa14 = new ObjectData("14", "Sarah", "DKI Jakarta", "Islam" , ["Sepeda" , "Badminton"], "Perempuan")
+var mahasiswa15 = new ObjectData("15", "Tya", "DKI Jakarta", "Islam" , ["Sepeda" , "Basket"], "Perempuan")
+var mahasiswa16 = new ObjectData("16", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa17 = new ObjectData("17", "Hartono", "Bekasi", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
+var mahasiswa18 = new ObjectData("18", "Fadil", "Bogor", "Islam" , ["Basket" , "Renang"], "Laki-Laki")
+var mahasiswa19 = new ObjectData("19", "Revina", "Surabaya", "Islam" , ["Sepeda" , "Renang"], "Perempuan")
+var mahasiswa20 = new ObjectData("20", "Syifa", "Malang", "Islam" , "Renang", "Perempuan")
+var mahasiswa21 = new ObjectData("21", "Ali", "Bekasi", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
+var mahasiswa22 = new ObjectData("22", "Nia", "Bandung", "Islam" , ["Sepeda" , "Renang"], "Perempuan")
+var mahasiswa23 = new ObjectData("23", "Zazam", "Semarang", "Islam" , ["Sepeda" , "Basket", "Badminton"], "Laki-Laki")
+var mahasiswa24 = new ObjectData("24", "Denny", "Bogor", "Islam" , ["Sepeda" , "Badminton"], "Laki-Laki")
+var mahasiswa25 = new ObjectData("25", "Dina", "Bogor", "Islam" , ["Sepeda" , "Badminton"], "Laki-Laki")
+var mahasiswa26 = new ObjectData("26", "Riady", "Bekasi", "Islam" , ["Sepeda" , "Renang", "Basket"], "Laki-Laki")
+var mahasiswa27 = new ObjectData("27", "Cinta", "Pekalongan", "Islam" , ["Sepeda" , "Renang" , "Football"], "Perempuan")
+var mahasiswa28 = new ObjectData("28", "Laras", "Madiun", "Islam" , ["Sepeda" , "Renang"], "Laki-Laki")
+var mahasiswa29 = new ObjectData("29", "Sarah", "DKI Jakarta", "Islam" , ["Sepeda" , "Badminton"], "Perempuan")
+var mahasiswa30 = new ObjectData("30", "Tya", "DKI Jakarta", "Islam" , ["Sepeda" , "Basket"], "Perempuan")
+var mahasiswa31 = new ObjectData("31","Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa32 = new ObjectData("32", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa33 = new ObjectData("33", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa34 = new ObjectData("34", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa35 = new ObjectData("35", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa36 = new ObjectData("36", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa37 = new ObjectData("37", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+var mahasiswa38 = new ObjectData("38", "Asep Syafrudin", "DKI Jakarta", "Islam" , ["Badminton" , "Renang"], "Laki-Laki")
+
 
 globalData.push(mahasiswa1);
 globalData.push(mahasiswa2);
@@ -71,18 +81,27 @@ globalData.push(mahasiswa27);
 globalData.push(mahasiswa28);
 globalData.push(mahasiswa29);
 globalData.push(mahasiswa30);
+globalData.push(mahasiswa31);
+globalData.push(mahasiswa32);
+globalData.push(mahasiswa33);
+globalData.push(mahasiswa34);
+globalData.push(mahasiswa35);
+globalData.push(mahasiswa36);
+globalData.push(mahasiswa37);
+globalData.push(mahasiswa38);
+
 
 //make the pagination 
-var currentPage = 1;
-var total_data_per_page = 5;
+let currentPage = 1;
+let total_data_per_page = 5;
 
-var pagination = (e=1) => {
-    var page = ""
-    var totalPages = Math.ceil(globalData.length/total_data_per_page);
-    var maxPagesShow = 3;
+const pagination = (e=1, data = globalData) => {
+    let page = ""
+    let totalPages = Math.ceil(data.length/total_data_per_page);
+    let maxPagesShow = 3;
     currentPage = e;
-    var startPage = 0;
-    var endPage = 0;
+    let startPage = 0;
+    let endPage = 0;
     
     if(totalPages > 0) {
         if (totalPages <= maxPagesShow) {
@@ -105,35 +124,35 @@ var pagination = (e=1) => {
                 // current page somewhere in the middle
                 startPage = currentPage - maxPagesBeforeCurrentPage;
                 endPage = currentPage + maxPagesAfterCurrentPage;
-            }
+        }
         }
         page += `
             <li class="page-item">
-                    <a class="page-link" onclick="prevpage()" tabindex="-1">Previous</a>
+                    <a class="page-link" onclick="prevpage(checkFilter())" tabindex="-1">Previous</a>
             </li>
         `
     
         for (let i = startPage ; i <= endPage; i++) {
             page += `
-            <li class="page-item"><a class="page-link" onclick="newshow(${i})">${i}</a></li>
+            <li class="page-item"><a class="page-link" onclick="newshow(${i}, checkFilter())">${i}</a></li>
             `
         }  
         page += `
             <li class="page-item">
-                    <a class="page-link" onclick="nextpage()" tabindex="-1">Last</a>
+                    <a class="page-link" onclick="nextpage(checkFilter())" tabindex="-1">Last</a>
             </li>
         `
         document.querySelector(".pagination").innerHTML = page;
     }
 }
 
-var nextpage = () => {
-        currentPage = Math.ceil(globalData.length / total_data_per_page);
-        newshow(currentPage);
+const nextpage = (data = globalData) => {
+        currentPage = Math.ceil(data.length / total_data_per_page);
+        newshow(currentPage , data);
 }
 
-var prevpage = () => {
-        newshow(1);
+const prevpage = (data = globalData) => {
+        newshow(1 , data);
 }
 // fn to generate row table from data
 
@@ -141,49 +160,68 @@ const getEl = el => {
     return document.querySelector(el)
 }
 
-const showData = (dataShow = globalData) => {
-    const table = getEl("table[type=listData] tbody")
+// const showData = (dataShow = globalData) => {
+//     const table = getEl("table[type=listData] tbody")
     
-    const listData = dataShow.map((value, index) => {
-        return `
-        <tr>
-        <th scope="row">${index + 1}</th>
-        <td>${value.name}</td>
-                <td>${value.address}</td>
-                <td>${value.religion}</td>
-                <td>${value.hobby}</td>
-                <td>${value.gender}</td>
-                <td align="center">
-                    <button index="${index}" onclick="updateData(this)">Ubah</button>
-                </td>
-                <td align = "center">
-                    <button index="${index}" onclick="deleteData(this)">Hapus</button>
-                </td>
-            </tr>
-        `
-    })
+//     const listData = dataShow.map((value, index) => {
+//         return `
+//         <tr>
+//         <th scope="row">${index + 1}</th>
+//         <td>${value.name}</td>
+//                 <td>${value.address}</td>
+//                 <td>${value.religion}</td>
+//                 <td>${value.hobby}</td>
+//                 <td>${value.gender}</td>
+//                 <td align="center">
+//                     <button index="${index}" onclick="updateData(this)">Ubah</button>
+//                 </td>
+//                 <td align = "center">
+//                     <button index="${index}" onclick="deleteData(this)">Hapus</button>
+//                 </td>
+//             </tr>
+//         `
+//     })
 
-    table.innerHTML = listData.join("")
-}
+//     table.innerHTML = listData.join("")
+// }
 
 //show data
 // fn to save data
-const saveData = async () => {
+let StartIdUser = globalData.length+1;
+const saveData = async (data = globalData) => {
     event.preventDefault()
 
     const form = document.inputData
-    const indexData = form.index.value ? form.index.value : globalData.length
     // if ? value true : value false
+    let idUser = form.idUser.value ? form.idUser.value : StartIdUser ;
 
-    const updatedData = {
-        name: form.name.value,
-        address: form.address.value,
-        religion: form.religion.value,
-        hobby: getValueCheckbox("hobby[]"),
-        gender: form.gender.value
+    if (idUser == form.idUser.value) {
+        for (let index = 0; index < globalData.length; index++) {
+            if(globalData[index].id == idUser) {
+                console.log(updateData);
+                globalData[index].name = form.name.value;
+                globalData[index].address = form.address.value;
+                globalData[index].religion = form.religion.value;
+                globalData[index].hobby = getValueCheckbox("hobby[]");
+                globalData[index].gender = form.gender.value;
+
+                form.idUser.removeAttribute('value');
+                break;
+            } 
+        }
+    } else {
+        let newData = new ObjectData(
+            idUser , 
+            form.name.value , 
+            form.address.value , 
+            form.religion.value , 
+            getValueCheckbox("hobby[]"), 
+            form.gender.value
+        )
+        await globalData.push(newData);
     }
-    await globalData.splice(indexData, 1, updatedData);
-    newshow(currentPage);
+
+    newshow(currentPage, data);
     form.reset()
 }
 
@@ -205,13 +243,14 @@ const setValueCheckbox = (el, values) => {
 }
 
 // set form's values
-const updateData = el => {
+const updateData = (el, data=globalData) => {
     el.closest("tr").style.backgroundColor = "lightgrey"
 
     const indexData = el.getAttribute("index")
-    const selectedData = globalData[indexData]
+    const selectedData = data[indexData]
     const form = document.inputData
 
+    form.idUser.value = selectedData.id
     form.name.value = selectedData.name
     form.gender.value = selectedData.gender
     form.religion.value = selectedData.religion
@@ -222,69 +261,97 @@ const updateData = el => {
 }
 
 // delete data
-const deleteData = async (el) => {
-    const indexData = el.getAttribute("index") 
-    await globalData.splice(indexData, 1)
-    var totalPages = Math.ceil(globalData.length/total_data_per_page);
+const deleteData = async (el , data = globalData) => {
+    const idData = el.id;
+    for (let index = 0; index < globalData.length; index++) {
+        if (globalData[index].id == idData) {
+            await globalData.splice(index , 1);
+        }        
+    }
+
+    for (let index = 0; index < data.length; index++) {
+        if (data[index].id == idData) {
+            await data.splice(index , 1);
+        }     
+        
+    }
+    var totalPages = Math.ceil(data.length/total_data_per_page);
     
     if(currentPage >= totalPages) {
         currentPage = totalPages;
-        newshow(currentPage);
+        newshow(currentPage, data);
     } else {
-        newshow(currentPage);
+        newshow(currentPage, data);
     }
 }
 
 // fn to filter data using typed value
 const searchData = self => {
-    const valueSearch = self.value
+    const valueSearch = self.value.toLowerCase();
 
     const filteredData = globalData.filter(value => {
-        return value.name.includes(valueSearch) || value.address.includes(valueSearch)
+        const nameData = value.name.toLowerCase();
+        const addressData = value.name.toLowerCase();
+        return nameData.includes(valueSearch) || addressData.includes(valueSearch)
     })
-    showData(filteredData)
+    newshow(1,filteredData)
+    return filteredData;
 }
 
-var show5data = function (e) {
-    if (e > 0) {
-        console.log(e)
+//function utama untuk menampilkan data
+const show5data = function (page , data = globalData) {
+    if (page > 0) {
         var total_tr ="";
-        for(index = (e-1)*total_data_per_page; index<(e * total_data_per_page) && index < globalData.length; index++) {
+        for(index = (page-1)*total_data_per_page; index<(page * total_data_per_page) && index < data.length; index++) {
             
             total_tr += `
             <tr>
             <th scope="row">${index + 1}</th>
-            <td>${globalData[index].name}</td>
-            <td>${globalData[index].address}</td>
-            <td>${globalData[index].religion}</td>
-            <td>${globalData[index].hobby}</td>
-            <td>${globalData[index].gender}</td>
+            <td>${data[index].name}</td>
+            <td>${data[index].address}</td>
+            <td>${data[index].religion}</td>
+            <td>${data[index].hobby}</td>
+            <td>${data[index].gender}</td>
             <td align="center">
-            <button index="${index}" onclick="updateData(this)">Ubah</button>
+            <button index="${index}" onclick="updateData(this, checkFilter())">Ubah</button>
             </td>
             <td align = "center">
-                <button index="${index}" onclick="deleteData(this)">Hapus</button>
+                <button index="${index}" id="${data[index].id}" onclick="deleteData(this, checkFilter())">Hapus</button>
                 </td>
                 </tr>
                 `   
             }
+            pagination(page, data);
     }
     return total_tr;
 }
 
-async function newshow(e) {
+//function untuk menampilkan data dimana membutuhkan parameter page dan data yang akan di render
+function newshow(page , data) {
     const table = getEl("table[type=listData] tbody")
-    if(show5data(e) === undefined) {
+    if(show5data(page , data ) === undefined) {
         table.innerHTML =""
-        await pagination(e);
     } else {
-        table.innerHTML = show5data(e); 
-        await pagination(e);
+        table.innerHTML = show5data(page, data); 
+        
     }
 }
+
+//menginialisasi data awal yang akan di tampilkan merujuk ke halaman 1
+function init() {
+    newshow(1)
+}
+init() //show the first page
     
-newshow(1) //show the first page
-    
+/*untuk mengecek apakah search box berisi suatu nilai 
+untuk di cari sehingga data yang di tampilkan pada pagination bisa sesuai 
+**/
+
+function checkFilter(){
+    let key = document.querySelector("#mySearch");
+    let arraySearch = searchData(key);
+    return arraySearch;
+}
 
 /**
  * TUGAS:
